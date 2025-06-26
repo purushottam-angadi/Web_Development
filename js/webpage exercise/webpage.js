44caefc17e5e355b2bc15631fd518c0d33e75e2d
purushottam-angadi <purushottam.angadi21@gmail.com>
2025-06-26 12:28:08 +0530

    commit

--- js/webpage exercise/webpage.js
+++ js/webpage exercise/webpage.js
@@ -1,3 +1,37 @@
+function createcard(title,cName,views,months,duration,thumbnail){
+
+    let viewstr
+    if (views<100000)
+    {
+         viewstr=`${views}K`;
+    }
+    else if(views>1000000)
+        {
+             viewstr=`${views/1000000}M`;
+    }
+    else 
+    {
+           viewstr=`${views/1000}K`;
+    }
+
+    
+let html=` <div class="card">
+
+            <div class="image">
+                <img src="${thumbnail}"
+                    alt="">
+                     <div class="capsule">${duration}</div>
+            </div>
+            <div class="text">
+                <h3>${title}</h3>
+                <p>${cName}. ${viewstr} views . ${months} months old </p>
+            </div>
+
+        </div>`
+        document.querySelector(".container").innerHTML=document.querySelector(".container").innerHTML + html;
 
 
-let 
+
+}
+createcard("Installing VS Code &How Websites Work| Sigma Web Development","Puru",10000000,2,"12:06","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLB6VzxANxt3dHm91HW9lLfDlxvmuA");
+createcard("Installing VS Code &How Websites Work| Sigma Web Development","Puru",100000,2,"12:06","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLB6VzxANxt3dHm91HW9lLfDlxvmuA");
